53b16c16c2799d56d36a2553547a063e2bb0bde2
Kim Jae Hyeok <master_catch@naver.com>
2024-10-16 06:03:00 +0900

    [JAVA] 1일차 실전예제 (2024-10-16)

--- src/example/Example01.java
+++ src/example/Example01.java
@@ -10,10 +10,19 @@ public class Example01 {
 		int tNum = 10;
 		double pNum = 2.0;
 		
+		System.out.println("덧셈 결과: " + (tNum + pNum));
+		System.out.println("뺄셈 결과: " + (tNum - pNum));
+		System.out.println("곱셈 결과: " + (tNum * pNum));
+		System.out.println("나누기 결과: " + (tNum / pNum));
+		
 		System.out.println("=======================");
 		
 		// 2. '글'이라는 한글 문자의 유니코드 값을 찾아서 char형으로 선언한 변수에 저장한 뒤 그 변수를 출력하여 확인해 보세요.
-
+		String myTxt = "글";
+//		char chTxt = (char)"글";
+		
+		System.out.println("String 결과: " + myTxt);
+//		System.out.println("char 반환결과: " + chTxt);
 	}
 
 }
