9dc73df3efe3dcbfa48bde1169ee24710edc4019
Kim Jae Hyeok <master_catch@naver.com>
2024-10-15 10:28:14 +0900

    Initial commit

---
+++ src/practice/practice01.java
@@ -0,0 +1,12 @@
+package practice;
+
+public class practice01 {
+
+	public static void main(String[] args) {
+		
+		// Day1
+		System.out.println("Day1 공부시작");
+		System.out.println("=======================");
+	}
+
+}
