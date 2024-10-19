9efc50a4b9496955100b2acf01f2644f277c037e
Kim Jae Hyeok <master_catch@naver.com>
2024-10-19 19:39:42 +0900

    Person, PersonTest 클래스 추가

---
+++ src/lonelyPlace/constructor/Person.java
@@ -0,0 +1,16 @@
+package lonelyPlace.constructor;
+
+public class Person {
+	String name;
+	float height; // float : 실수 자료형
+	float weight;
+	
+	public Person() {} // 디폴트 생성자
+	
+	// 이름을 매개변수(Parameter)로 입력받는 생성자 (매개변수 : 함수 선언 시 함수가 받아들일 값)
+	public Person(String pname, float pheight, float pweight) {
+		name = pname;
+		height = pheight;
+		weight = pweight;
+	}
+}
---
+++ src/lonelyPlace/constructor/PersonTest.java
@@ -0,0 +1,14 @@
+package lonelyPlace.constructor;
+
+// 클래스
+public class PersonTest {
+	// 메소드
+	public static void main(String[] args) {
+		Person personKim = new Person(); // Person = 생성자
+		personKim.name = "김유신";
+		personKim.weight = 85.5F;
+		personKim.height = 180.0F; 
+		
+		Person personLee = new Person("이순신", 175, 75); 
+	}
+}
