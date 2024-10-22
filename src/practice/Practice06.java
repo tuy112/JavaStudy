a281b70b9a8a4f0e719a24477783707411899028
Kim Jae Hyeok <master_catch@naver.com>
2024-10-22 10:28:38 +0900

    [JAVA] 6일차 공부 중

---
+++ src/practice/Practice06.java
@@ -0,0 +1,19 @@
+package practice;
+
+public class Practice06 {
+
+	public static void main(String[] args) {
+		System.out.println("공부시작");
+		System.out.println("=======================");
+		// 1. 배열
+		int [] num = new int[] { 1,2,3,4,5,6,7,8,9,10 };
+		for (int i = 0; i < num.length; i++) {
+			System.out.println(num[i]);
+		}
+		
+		System.out.println("=======================");
+		
+		// 2. 
+	}
+
+}
